6e68cf9a9d1991c08656ed4ce4cc416808ea7cdf
Andrei Vasiliu <whyte.vuhuni@gmail.com>
2021-12-29 12:53:18 +0200

    Add basic ingress controller

---
+++ demikube_ingress.py
@@ -0,0 +1,101 @@
+#!/var/demi/k8s/venv/bin/python3
+
+from typing import (
+    NamedTuple, Optional, List, Union, Literal, Iterator, NoReturn, Dict,
+)
+import logging
+import subprocess
+import itertools
+import os
+
+import kubernetes
+
+
+log = logging.getLogger('demikube-ingress')
+
+
+class IngressRule(NamedTuple):
+    host: Optional[str]
+    path_type: Literal['Exact']
+    path: str
+    service_name: str
+    service_port: Union[int, str]
+
+
+class Ingress(NamedTuple):
+    name: str
+    namespace: str
+    default_backend: Optional[str]
+    rules: List[IngressRule]
+
+
+def ingress_events() -> Iterator[Ingress]:
+    watch = kubernetes.watch.Watch()
+    net_v1 = kubernetes.client.NetworkingV1Api()
+
+    for event in watch.stream(net_v1.list_ingress_for_all_namespaces):
+        event_type = event['type']
+        ingress = event['object']
+
+        yield event_type, Ingress(
+            name=ingress.metadata.name,
+            namespace=ingress.metadata.namespace,
+            default_backend=ingress.spec.default_backend,
+            rules=[
+                IngressRule(
+                    host=rule.host,
+                    path_type=path.path_type,
+                    path=path.path,
+                    service_name=path.backend.service.name,
+                    service_port=path.backend.service.port.name or path.backend.service.port.number,
+                )
+                for rule in ingress.spec.rules
+                for path in rule.http.paths
+            ]
+        )
+
+
+def write_ingresses(ingresses: Dict[str, Ingress]) -> None:
+    rules = sorted(
+        (rule for ingress in ingresses.values() for rule in ingress.rules),
+        key=lambda rule: -len(rule.path)
+    )
+
+    with open('/etc/nginx-ingresses.conf.tmp', 'w') as f:
+        for host, host_rules in itertools.groupby(rules, lambda rule: rule.host):
+            # FIXME: Sanitize
+            for rule in host_rules:
+                f.write(f'"{host}/{rule.path}"\t"{rule.service_name}:{rule.service_port}";\n')
+        
+    os.rename('/etc/nginx-ingresses.conf.tmp', '/etc/nginx-ingresses.conf')
+
+
+def reload_nginx() -> None:
+    subprocess.check_call([
+        '/usr/bin/systemctl', 'reload', 'nginx'
+    ])
+
+
+def main_loop() -> NoReturn:
+    kubernetes.config.load_kube_config("/var/demi/k8s/demikube-ingress.kubeconfig")
+
+    ingresses = {}
+
+    for event_type, ingress in ingress_events():
+        log.info(f"Ingress {ingress.namespace}/{ingress.name} {event_type}")
+
+        if event_type in ('ADDED', 'MODIFIED'):
+            ingresses[f"{ingress.namespace}/{ingress.name}"] = ingress
+        elif event_type == 'DELETED':
+            del ingresses[f"{ingress.namespace}/{ingress.name}"]
+        else:
+            log.warning("Ignored unknown event type %s", event_type)
+            continue
+
+        write_ingresses(ingresses)
+        reload_nginx()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main_loop()
